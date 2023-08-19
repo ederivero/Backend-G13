d0b19d0392148b6428c2b5ad367db66f6c2dac5d
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2023-08-18 23:08:56 -0500

    crud completo

--- dulceria/gestion/urls.py
+++ dulceria/gestion/urls.py
@@ -1,10 +1,12 @@
 # aca declararemos todas las rutas pertenecientes a esta aplicacion
 from django.urls import path
 # cuando colocamos '.' indicamos que se trata de un archivo en el mismo nivel, sin embargo si no colocamos el '.' estaremos indicando que sera o una libreria o un archivo externo
-from .views import paginaInicio, devolverHoraServidor, CategoriasController
+from .views import paginaInicio, devolverHoraServidor, CategoriasController, CategoriaController
 
 urlpatterns = [
     path('inicio', paginaInicio),
     path('status', devolverHoraServidor),
     path('categorias', CategoriasController.as_view()),
+    path('categoria/<id>', CategoriaController.as_view())
+
 ]
--- dulceria/gestion/views.py
+++ dulceria/gestion/views.py
@@ -50,9 +50,11 @@ class CategoriasController(APIView):
         # SELECT * FROM categorias;
         categorias = CategoriaModel.objects.all()
         print(categorias)
+        serializador = CategoriaSerializer(instance=categorias, many=True)
 
         return Response(data={
-            'message': 'La categoria es`'
+            'message': 'La categoria es`',
+            'content': serializador.data
         })
 
     def post(self, request: Request):
@@ -74,3 +76,55 @@ class CategoriasController(APIView):
                 'message': 'Error al crear la categoria',
                 'content': serializador.errors  # me da un listado con los errores
             }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CategoriaController(APIView):
+    def get(self, request: Request, id: str):
+        categoriaEncontrada = CategoriaModel.objects.filter(id=id).first()
+        if not categoriaEncontrada:
+            return Response(data={
+                'message': 'Categoria no encontrada'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        serializador = CategoriaSerializer(instance=categoriaEncontrada)
+        return Response(data={
+            'content': serializador.data
+        })
+
+    def put(self, request: Request, id: str):
+        categoriaEncontrada = CategoriaModel.objects.filter(id=id).first()
+        if not categoriaEncontrada:
+            return Response(data={
+                'message': 'Categoria no encontrada'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        data = request.data
+        serializador = CategoriaSerializer(data=data)
+        dataValida = serializador.is_valid()
+        if dataValida:
+            serializador.validated_data  # la data convertida a un diccionario con los campos que necesita el modelo, si se le llegase a pasar algun campo que no utiliza este automaticamente no se guardaria en este atributo
+            serializador.update(categoriaEncontrada,
+                                serializador.validated_data)
+
+            return Response(data={
+                'message': 'Categoria actualizada exitosamente'
+            })
+
+        else:
+            return Response(data={
+                'message': 'Error al actualizar la categoria',
+                'content': serializador.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, id: str):
+        categoriaEncontrada = CategoriaModel.objects.filter(id=id).first()
+        if not categoriaEncontrada:
+            return Response(data={
+                'message': 'Categoria no encontrada'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        CategoriaModel.objects.filter(id=id).delete()
+
+        return Response(data={
+            'message': 'Categoria eliminada exitosamente'
+        })
