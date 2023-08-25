29b4c394503dc36964f14e517dded6e6a485b188
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2023-08-24 20:37:51 -0500

    agregue whitenoise

--- dulceria/dulceria/settings.py
+++ dulceria/dulceria/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,3 +135,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# es donde se almacenaran los archivos generados por el comando collecstatic
+STATIC_ROOT = BASE_DIR / 'archivos_estaticos'
